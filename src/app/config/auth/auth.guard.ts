ea60d4e2d85911d716963163836ced13c9649e23
m.urbaniak <urbaniak.michal@yahoo.com>
2024-03-10 18:01:06 +0100

    add auth guards canActivate to some endpoints

---
+++ src/app/config/auth/auth.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from "@angular/core";
+import { CanActivate, Router } from "@angular/router";
+import { AuthService } from "./auth.service";
+import { ToastService } from "../../features/toast.service";
+
+@Injectable({
+  providedIn: "root"
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private toastService: ToastService
+  ) {
+  }
+
+  canActivate(): boolean {
+    if (this.authService.isLoggedIn()) {
+      return true;
+    } else {
+      this.router.navigate(["/login"])
+        .then(nav => this.toastService.toastInfo("Redirect to login page"),
+          error => this.toastService.toastError(error));
+
+      return false;
+    }
+  }
+}
